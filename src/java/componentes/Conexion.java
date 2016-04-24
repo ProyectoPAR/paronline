4f1a641d7a02fae2240d9e58bb9168d967618c87
Fabricio <fabromendoza95@gmail.com>
2016-04-24 01:04:14 -0400

    Hasta ahora se han creado tres servlets, tres paginas jsp, una clase para gestionar la conexion

---
+++ src/java/componentes/Conexion.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author root
+ */
+package componentes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+public class Conexion {
+    public static String url = "jdbc:postgresql://localhost:5432/paronline";
+    public static String user = "postgres";
+    public static String pass = "sate150495";
+    public static Connection conexion = null;
+    public static PreparedStatement sentencia = null;
+    public static ResultSet result = null;
+    
+    public Conexion(String url, String user, String pass)throws SQLException, ClassNotFoundException{
+        Class.forName("org.postgresql.Driver");
+        this.conexion = DriverManager.getConnection(url,user, pass);
+    }
+
+    
+    public static void consultar(String query, String[] args) throws SQLException, ClassNotFoundException{
+        Class.forName("org.postgresql.Driver");
+        conexion = DriverManager.getConnection(url,user, pass);
+        sentencia = conexion.prepareStatement(query);
+        for(int i = 0 ; i < args.length ; i ++){
+            sentencia.setString(i, args[i+1]);
+        }
+        result = sentencia.executeQuery();
+    }
+    
+    public static void consultar(String query) throws SQLException, ClassNotFoundException{
+        Class.forName("org.postgresql.Driver");
+        conexion = DriverManager.getConnection(url,user, pass);
+        sentencia = conexion.prepareStatement(query);
+        result = sentencia.executeQuery();
+    }
+    
+    public static ResultSet getResult(){
+        return result;
+    }
+    
+    public static void cerrarConexion() throws SQLException{
+        result.close();
+        sentencia.close();
+        conexion.close();
+    }
+        
+}
